0646ed07fbd4d6c25206ec368c5b1cea77eaa733
Kaladar <ckaladar@gmail.com>
2022-08-02 14:12:23 +0530

    Function to generate prime until given number

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello, World");
+function addToPreviousPrimesArray(previousPrimesArray, primeNumber) {
+  previousPrimesArray.push(primeNumber);
+}
+
+function isDivisibleByAnyPreviousPrimes(previousPrimesArray, num) {
+  for (const i of previousPrimesArray) {
+    if (num % i === 0) return true;
+  }
+  return false;
+}
+
+function generatePrimesFrom2ToUntil(num) {
+  previousPrimesArray = [];
+
+  addToPreviousPrimesArray(previousPrimesArray, 2);
+  addToPreviousPrimesArray(previousPrimesArray, 3);
+
+  for (let i = 5; i <= num; i += 2) {
+    if (!isDivisibleByAnyPreviousPrimes(previousPrimesArray, i)) {
+      addToPreviousPrimesArray(previousPrimesArray, i);
+    }
+  }
+
+  return previousPrimesArray;
+}
+
+console.log(generatePrimesFrom2ToUntil(100));
